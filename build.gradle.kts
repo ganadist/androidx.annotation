a99f4a0a642b4198d10932d4a3382906f8ceef1a
YOUNG HO CHA <ganachoco@linecorp.com>
2024-05-04 23:06:41 +0900

    Setup build

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("org.jetbrains.kotlin.multiplatform") version "1.9.23"
+}
+
+kotlin {
+    jvm().compilations.all {
+        compilerOptions.configure {
+            jvmTarget.set(JvmTarget.JVM_1_8)
+        }
+    }
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                api("org.jetbrains.kotlin:kotlin-stdlib:1.9.23")
+            }
+        }
+    }
+}
